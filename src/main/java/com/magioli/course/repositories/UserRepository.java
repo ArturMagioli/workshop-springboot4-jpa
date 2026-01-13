5a765e6908d47679db0b4575a086da242c733127
Artur Magioli <arturdepaula00@gmail.com>
2026-01-12 21:03:10 -0300

    JPA repository, dependency injection, databse seeding

---
+++ src/main/java/com/magioli/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.magioli.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.magioli.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
