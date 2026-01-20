54e823b188e0ffbd326fe084faed1df56213690b
Artur Magioli <arturdepaula00@gmail.com>
2026-01-20 17:59:35 -0300

    Payment one-to-one association

--- src/main/java/com/magioli/course/config/TestConfig.java
+++ src/main/java/com/magioli/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.magioli.course.entities.Order;
 import com.magioli.course.entities.OrderItem;
+import com.magioli.course.entities.Payment;
 import com.magioli.course.entities.Product;
 import com.magioli.course.entities.User;
 import com.magioli.course.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-07-21T05:42:10Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
