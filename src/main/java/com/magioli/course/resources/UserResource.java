479b436b7dcae6d60c1d1fe15fde68cc80aa4890
Artur Magioli <arturdepaula00@gmail.com>
2026-01-21 20:02:18 -0300

    User update

--- src/main/java/com/magioli/course/resources/UserResource.java
+++ src/main/java/com/magioli/course/resources/UserResource.java
@@ -17,6 +17,8 @@ import com.magioli.course.services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -49,5 +51,11 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> putMethodName(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
