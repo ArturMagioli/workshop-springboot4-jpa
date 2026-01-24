b07153d4bf80e7f3b05a0753780dd76c54ac3c12
Artur Magioli <arturdepaula00@gmail.com>
2026-01-24 15:25:59 -0300

    Exception handling - delete

---
+++ src/main/java/com/magioli/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.magioli.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
